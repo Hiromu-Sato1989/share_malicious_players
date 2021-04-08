85e801e5891edc013f88dfb7e383d3e0806b3dd8
Hiromu Sato <a.string.d@gmail.com>
2021-04-08 11:47:57 +0900

    categoriesテーブル作成とテスト記述 #9

---
+++ spec/factories/categories.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :category do
+    sequence(:name) { |n| "name_#{n}" }
+  end
+end
---
+++ spec/models/category_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe Category, type: :model do
+  describe 'カテゴリーを新規登録する' do
+    it 'カテゴリー1件登録が成功する' do
+      category = create(:category)
+      expect(category).to be_valid
+      expect(category.errors).to be_empty
+    end
+
+    it '別名のカテゴリー2件登録が成功する' do
+      category = create(:category)
+      category_another_name = create(:category)
+      expect(category).to be_valid
+      expect(category.errors).to be_empty
+      expect(category_another_name).to be_valid
+      expect(category_another_name.errors).to be_empty
+    end
+
+    it '同名のカテゴリー2件登録が失敗する' do
+      category = create(:category, name: 'name')
+      category_equal_name = build(:category, name: 'name')
+      expect(category).to be_valid
+      expect(category.errors).to be_empty
+      expect(category_equal_name).to be_invalid
+      expect(category_equal_name.valid?).to eq(false)
+    end
+  end
+end
