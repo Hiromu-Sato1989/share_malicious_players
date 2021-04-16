23a3e9f73d91a9114036af02026c2c2ef2a0eafd
Hiromu Sato <a.string.d@gmail.com>
2021-04-16 11:43:22 +0900

    検索機能システムスペック

--- spec/factories/fighters.rb
+++ spec/factories/fighters.rb
@@ -3,10 +3,20 @@
 FactoryBot.define do
   factory :fighter do
     sequence(:name) { |n| "name_#{n}" }
+    league_id { 1 }
     association :league
 
     trait :daigo do
       name { 'umehara' }
+
+      after(:create) do |fighter|
+        create_list(:character, 3, fighters: [fighter])
+      end
     end
+
+    # after(:create) do |fighter|
+    #   create_list(:character, 3, fighters: [fighter])
+    #   create_list(:category, 3, fighters: [fighter])
+    # end
   end
 end
