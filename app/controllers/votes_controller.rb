257c2cd66881a706c76d64e9000a0cf19903b8a0
Hiromu Sato <a.string.d@gmail.com>
2021-04-16 20:19:15 +0900

    votes機能作成 #21

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class VotesController < ApplicationController
+  def create
+    @fighter = Fighter.find(params[:fighter_id])
+    @alreadyvote = Vote.find_by(ip: request.remote_ip, fighter_id: params[:fighter_id])
+    if @alreadyvote
+      redirect_back fallback_location: root_path, danger: 'すでに投票しています'
+    else
+      @vote = Vote.create(fighter_id: params[:fighter_id], ip: request.remote_ip)
+      redirect_back fallback_location: root_path, success: '投票しました！'
+    end
+  end
 end
