9015ce74201f79203897f937abe6698641fa8f5d
Hiromu Sato <a.string.d@gmail.com>
2021-04-24 10:14:31 +0900

    投票数上位者のパーシャル #29

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 class TopController < ApplicationController
-  def index; end
+  def index
+    @fighters = Fighter.all.order(votes_count: :desc).limit(5)
+    @votes = Vote.all
+  end
 end
