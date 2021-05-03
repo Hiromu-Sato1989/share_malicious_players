12fd6724819a562b8d214f4dd6c9c45fb6c97440
Hiromu Sato <a.string.d@gmail.com>
2021-05-03 22:06:22 +0900

    Vue.js導入

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,6 @@
 const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
 
 const webpack = require('webpack')
 
@@ -8,4 +10,6 @@ environment.plugins.append('Provide', new webpack.ProvidePlugin({
   Popper: ['popper.js', 'default']
 }))
 
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
 module.exports = environment
