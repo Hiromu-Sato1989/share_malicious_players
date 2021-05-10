c839689afece0793623d2420990a1fad8b9e7e77
Hiromu Sato <a.string.d@gmail.com>
2021-05-10 18:28:39 +0900

    投票機能ajax #38

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,13 +2,15 @@
 
 class VotesController < ApplicationController
   def create
+    # 投票時にどのファイターか探す
     @fighter = Fighter.find(params[:fighter_id])
-    @alreadyvote = Vote.find_by(ip: request.remote_ip, fighter_id: params[:fighter_id])
-    if @alreadyvote
-      redirect_back fallback_location: root_path, danger: 'すでに投票しています'
-    else
-      @vote = Vote.create(fighter_id: params[:fighter_id], ip: request.remote_ip)
-      redirect_back fallback_location: root_path, success: '投票しました！'
-    end
+    # すでに投票していないかを探す
+    alreadyvote = Vote.find_by(ip: request.remote_ip, fighter_id: params[:fighter_id])
+    # 投票数に1を足す処理。非同期で投票数が動くようにするため
+    new_vote_balance = @fighter.votes_count + 1
+    # 見た目だけ加工してDBには保存しない
+    @fighter.assign_attributes(votes_count: new_vote_balance)
+    # 実際に投票が行われDBに保存される。リダイレクトすると投票数が1増えている
+    Vote.create(fighter_id: params[:fighter_id], ip: request.remote_ip) unless alreadyvote
   end
 end
